35f3cf499846fbe383e7181e232b7692404c8539
Denzy7 <dennismyner7@gmail.com>
2024-10-24 09:27:44 +0300

    v0.4 initial commit

---
+++ test-ci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+#include <pthread.h>
+
+void* thr(void* arg)
+{
+    printf("waiting. press enter\n");
+    getchar();
+    printf("exiting...\n");
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t;
+    if(pthread_create(&t, NULL, thr, NULL) < 0)
+    {
+        fprintf(stderr, "cannot init thread\n");
+        return 1;
+    }
+    pthread_join(t, NULL);
+    return 0;
+}
